9ea5c2aa1f1ee9de40b45661ca86167754367118
Erfan Moeini <moeini.erfan@gmail.com>
2023-05-14 21:19:22 +0330

    implement iranian and japanese components

---
+++ src/main/java/org/example/JapaneseFlower.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class JapaneseFlower implements AbstractFlower {
+}
--- src/test/java/AbstractFactoryUnitTest.java
+++ src/test/java/AbstractFactoryUnitTest.java
@@ -1,7 +1,6 @@
 package com.mechanitis.demo.junit5;
 
-import org.example.IranianGardenCreator;
-import org.example.JapaneseGardenCreator;
+import org.example.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
